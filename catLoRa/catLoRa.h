3d581e7fe6d5e4d1c35ca687a85960f2e0d1824a
iDemoLive <46700949+iDemoLive@users.noreply.github.com>
2019-08-05 13:49:00 +0700

    Update catLoRa.h

--- catLoRa/catLoRa.h
+++ catLoRa/catLoRa.h
@@ -2,9 +2,9 @@
 *
 * LORA BY CATTELECOM
 * 920 - 925 MHz (AS923)
-* Created on : April 22, 2019
+* Created on : August 04, 2019
 * For CAT StarterKit Gravitech S767
-* Created by : if2u
+* Created by : if2u v.3
 *
 ***********************************************/
 
@@ -30,6 +30,8 @@ class catLoRa {
 		void macCommand(String _Comm);
 		bool sendPayload(String _port, String _payLoad);
 		String getDL();
+		String payloadDL();
+		String portDL();
 		String getTemp();
 		String getHumi();
 		String getLPPformat();
